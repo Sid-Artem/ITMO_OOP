42c350fe3cc66b4b589ba6b8bbc1d34aedd918d2
Сидоров Артем <sidoroffartemii@gmail.com>
2024-08-12 21:59:42 +0300

    Добавьте файлы проекта.

---
+++ Тест/Angle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тест
+{
+    internal class Angle
+    {
+
+        double gradus;
+        double min;
+        double sec;
+
+        public double Gradus
+        {
+            get
+            {
+                return gradus;
+
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public double Min
+        {
+            get
+            {
+                return min;
+
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+
+        public double Sec
+        {
+            get
+            {
+                return sec;
+
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+
+        public Angle(double gradus, double min, double sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+
+        }
+
+
+        public double ToRadians()
+        {
+            return (gradus + ((min + sec) / 60) / 60) * Math.PI / 180;
+        }
+
+    }
+}
---
+++ Тест/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тест
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Gradus = Convert.ToDouble(Console.ReadLine());
+            double Min= Convert.ToDouble(Console.ReadLine());
+            double Sec = Convert.ToDouble(Console.ReadLine());
+            Angle angle = new Angle(Gradus,Min,Sec);
+            double totalRadians = angle.ToRadians();
+            Console.WriteLine(totalRadians);
+            Console.ReadKey();
+        }
+      
+            
+    }
+
+
+
+}
